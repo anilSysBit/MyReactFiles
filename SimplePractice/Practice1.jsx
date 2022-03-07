6ead890e71c01d4384d9021fbe48e5068db999eb
Anil <anil.wagle808@gmail.com>
2022-03-07 14:29:21 +0545

    a new file add player created and players name has been mapped and showed in ordered list

--- SimplePractice/Practice1.jsx
+++ SimplePractice/Practice1.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Context1, MyContext } from './Context1';
 import Practice2 from './Practice2';
+import Addplayer from './Addplayer';
 
 const Practice1 = () => {
   return (
     <Context1>
         <Practice2/>
+        <Addplayer/>
     </Context1>
   )
 }
