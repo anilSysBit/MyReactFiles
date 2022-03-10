2d3ff39c3e9df52cb937eabe70bdbfd7cace8943
Anil <anil.wagle808@gmail.com>
2022-03-10 21:58:25 +0545

    React Router added and some information about react router is given on routerfile.jsx

---
+++ ReactRouter/RouterFiles.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+//we import the components of react router dom
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom'
+
+// importing the pages of websites and linking 
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
+
+// if you use the a herf tag then on every click the page reloads 
+// for preventing this we have Link method on react router you can put your links inside the Link
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
